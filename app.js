7da937bb763fad6cfc2bfa40d6ed892a41e2bcaa
Umer-Faruk <umer.gvt@gmail.com>
2021-09-21 19:27:59 +0530

    file namr chnages

--- app.js
+++ app.js
@@ -1,66 +1,68 @@
-require('dotenv').config();
-const pinataSDK = require('@pinata/sdk');
-const pinata = pinataSDK('299e5e307c02e3848d26', 'b5f4aa6a74b4b7308483b2302ab3a1a1d790d944200f005ac884746fa6d1e021');
-const axios = require('axios');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const fs = require('fs');
+const app = express();
+const port = process.env.PORT || "8000";
+const upf = require("./uploder");
+ 
 
-const FormData = require('form-data');
 
-exports.pinFileToIPFS=(fpath,name,description) =>{
-    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+app.set('view engine','ejs');
+ app.use(express.static(__dirname + '/public'));
+ app.use('/uploads', express.static('uploads'));
 
+
+ var storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, './uploads')
+   },
+   filename: function (req, file, cb) {
+     cb(null, file.originalname)
+   }
+})
+var upload = multer({ storage: storage })
+
+ 
+ 
+app.get("/",function(req,res){
+   res.render("form");
+});
+
+app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+   // req.file is the `profile-file` file
+   // req.body will hold the text fields, if there were any
+   console.log("your image file is uplodinf... start Looding");
+   console.log(JSON.stringify(req.file))
+   
+  
+   var ipfsr =  upf.pinFileToIPFS(req.file.path,req.body.name,req.body.description);
+   ipfsr.then(function(_responce){
+    console.log(_responce);
+    //deleting the file from upload directory
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.error(err)
+        
+      }
+    else{
+      console.log("delete file success fully");
+    }});
+    console.log("your image file uploded success fully");
+  res.render("result",{
+    name:req.body.name,
+    description: req.body.description,
+    filesrc: req.file.path,
+    URI: _responce.IpfsHash
+
+
+  });
+   });
      
-    let data = new FormData();
-    data.append('file', fs.createReadStream(fpath));
- 
-    return axios
-        .post(url, data, {
-            maxBodyLength: 'Infinity', //this is needed to prevent axios from erroring out with large files
-            headers: {
-                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-                pinata_api_key: process.env.API_Key,
-                pinata_secret_api_key: process.env.API_Secret
-            }
-        })
-        .then(function (response) {
-            //handle response here
-            //console.log(response);
-            console.log("https://ipfs.io/ipfs/"+response.data.IpfsHash);
-            var imgURI = "https://ipfs.io/ipfs/"+response.data.IpfsHash;
-            
-           return pinJSONToIPFS(JSON.stringify({
-                name: name,
-                description: description,
-                image: imgURI
-              }));
-             
-        })
-        .catch(function (error) {
-            //handle error 
-            console.log(error);
-        });
-};
+ })
 
- function pinJSONToIPFS(JSONBody){
-    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
-    return axios
-        .post(url, JSONBody, 
-            
-            {
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                pinata_api_key: process.env.API_Key,
-                pinata_secret_api_key: process.env.API_Secret
-            }
-        })
-        .then(function (response) {
-            //handle response here
-            console.log("https://ipfs.io/ipfs/"+response.data.IpfsHash);
-            return response.data;
-        })
-        .catch(function (error) {
-            //handle error here
-            console.log(error);
-        });
-};
  
+ 
+ app.listen(port, () => {
+   console.log(`Listening to requests on http://localhost:${port}`);});
+   
